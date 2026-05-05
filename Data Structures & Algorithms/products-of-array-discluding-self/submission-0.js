4e4f3915e1a0508de2d6a330d62dd85caa3d1c6e
tanmay-girdhar <tanmaygirdhar17@gmail.com>
2026-05-06 02:53:16 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+ const result = new Array(nums.length).fill(1);
+      let prefix =1;
+      for(let i =0;i<=nums.length-1;i++){
+          result[i]=prefix;
+          prefix*=nums[i];
+      }
+      let postfix=1;
+      for (let i =nums.length-1;i>=0;i--){
+           result[i]*=postfix;
+          postfix*=nums[i];
+      }
+      return result;
+    }
+}
